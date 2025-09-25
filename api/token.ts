faf410d6a4faa0e1de800c946bd08ebe08610626
cleverhuman <cleverhuman110@gmail.com>
2025-09-26 00:33:45 +0900

    udpate token summary and settings page

---
+++ api/token.ts
@@ -0,0 +1,25 @@
+import { api, BASE_URL, triggerTokenExpiration } from '@/lib/api-client';
+
+const ENDPOINT = '/reggie/api/v1/usage/tokens'
+
+export const getTokenUsage = async (page: number = 1,page_size: number,
+  search: string) => {
+  const response = await api.get(`${ENDPOINT}/`, {
+    params: { page: page.toString(), page_size: page_size.toString(), search: search },
+  });
+  return response;
+};
+
+export const getTokenUsagebyUser = async () => {
+  const response = await api.get(`${ENDPOINT}/user/`, {
+  });
+  return response;
+};
+
+export const getUserTokenSummary = async (page: number = 1,page_size: number,
+  search: string) => {
+  const response = await api.get(`${ENDPOINT}/usersummary/`, {
+    params: { page: page.toString(), page_size: page_size.toString(), search: search },
+  });
+  return response;
+};
